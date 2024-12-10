34e99160887ad5c5d1a43ec6ff0b3e47f10e8c23
Swcyenh <tranquanghuyk15@siu.edu.vn>
2024-12-10 15:11:48 +0700

    remove fibo

--- Api.py
+++ Api.py
@@ -17,14 +17,7 @@ def check_prime(number: int) -> Dict[str, bool]:
             return {"is_prime": False}
     return {"is_prime": True}
 
-@app.get("/get_fibonacci/{number}")
-def get_fibonacci(number: int) -> Dict[str, int]:
-    if number < 0:
-        return {"error": "Number must be non-negative"}
-    a, b = 0, 1
-    for _ in range(number):
-        a, b = b, a + b
-    return {"fibonacci": a}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
